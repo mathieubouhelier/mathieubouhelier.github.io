996e933de02143dd0e668c4f5e6808eed6351e26
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-05-26 22:50:56 -0300

    small changes in CSS shoping cart project

--- project-shopping-cart/script.js
+++ project-shopping-cart/script.js
@@ -36,6 +36,7 @@ async function sumPrice() {
   const cartListLi = document.querySelectorAll('.cart__item');
   let sum = 0;
   cartListLi.forEach(index => (sum += parseFloat(index.innerText.split('$')[1])));
+  sum = Math.round(sum * 100) / 100;
   document.querySelector('.total-price').innerText = sum;
 }
 
