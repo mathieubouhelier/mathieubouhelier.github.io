6febf4a49537bc5f8e5bf644c8c621b8a09c0b73
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-06-17 13:19:49 -0300

    add pokedex

---
+++ pokedex/precache-manifest.1d30d5acd40dece591a394399f93f497.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "712999d379ff1872959ec1ff2f3b91fe",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f4cce1d82963cc6b4e2c",
+    "url": "./static/css/main.4dbfa9af.chunk.css"
+  },
+  {
+    "revision": "bebf73cd6d1fca73ec85",
+    "url": "./static/js/2.84d5528e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.84d5528e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f4cce1d82963cc6b4e2c",
+    "url": "./static/js/main.0f97439d.chunk.js"
+  },
+  {
+    "revision": "0d690fa4df1bde609f3b",
+    "url": "./static/js/runtime-main.83c64bab.js"
+  },
+  {
+    "revision": "ab870dc2167d2f2a08c5c15370576e3f",
+    "url": "./static/media/1.ab870dc2.png"
+  },
+  {
+    "revision": "4398adb103b4997a2c24122cfd11c1e7",
+    "url": "./static/media/2.4398adb1.jpg"
+  }
+]);
