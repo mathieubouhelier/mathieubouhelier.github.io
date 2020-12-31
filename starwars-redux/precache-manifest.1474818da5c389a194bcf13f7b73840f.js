a0cf3f1fd5663b4d81ba184a36ae2a2d776fbc4f
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-12-31 16:13:06 -0300

    repository starwars-redux only homepage /index03

---
+++ starwars-redux/precache-manifest.1474818da5c389a194bcf13f7b73840f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3327177864bfe26675a4a817aa16ebad",
+    "url": "/starwars-redux/index.html"
+  },
+  {
+    "revision": "4eb5c774eb4c50f73bf2",
+    "url": "/starwars-redux/static/css/2.25d2a862.chunk.css"
+  },
+  {
+    "revision": "89486fd8bf079685a614",
+    "url": "/starwars-redux/static/css/main.10b93826.chunk.css"
+  },
+  {
+    "revision": "4eb5c774eb4c50f73bf2",
+    "url": "/starwars-redux/static/js/2.3df13ed2.chunk.js"
+  },
+  {
+    "revision": "3f6aa4d1b94e64ba93470af92b79dbf5",
+    "url": "/starwars-redux/static/js/2.3df13ed2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "89486fd8bf079685a614",
+    "url": "/starwars-redux/static/js/main.6db0583e.chunk.js"
+  },
+  {
+    "revision": "1bbea74d793c9d814080",
+    "url": "/starwars-redux/static/js/runtime-main.ea517e44.js"
+  },
+  {
+    "revision": "76de0b163a99133b02c88cdf7b24dccc",
+    "url": "/starwars-redux/static/media/deadstar.76de0b16.png"
+  }
+]);
