d6d8b09b4463e122e86dde1339f70a077e14e17d
mathieu bouhelier <mathieubouhelier@gmail.com>
2020-12-31 14:55:25 -0300

    Updates

---
+++ precache-manifest.71e6f41e5522e4e24f679b64b2904f9c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cefbf35435ec736e834c004ebd96663",
+    "url": "/redux-starwars-database-filters/index.html"
+  },
+  {
+    "revision": "5e59516a0f4423bda71f",
+    "url": "/redux-starwars-database-filters/static/css/2.25d2a862.chunk.css"
+  },
+  {
+    "revision": "c9deda94cd476652fbba",
+    "url": "/redux-starwars-database-filters/static/css/main.5ec21889.chunk.css"
+  },
+  {
+    "revision": "5e59516a0f4423bda71f",
+    "url": "/redux-starwars-database-filters/static/js/2.8a204cd4.chunk.js"
+  },
+  {
+    "revision": "b4f9955a46235ab5d9673fafd3d3f6c5",
+    "url": "/redux-starwars-database-filters/static/js/2.8a204cd4.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c9deda94cd476652fbba",
+    "url": "/redux-starwars-database-filters/static/js/main.f9acbfea.chunk.js"
+  },
+  {
+    "revision": "cfa92fed2acac5979405",
+    "url": "/redux-starwars-database-filters/static/js/runtime-main.09f2ab9e.js"
+  },
+  {
+    "revision": "76de0b163a99133b02c88cdf7b24dccc",
+    "url": "/redux-starwars-database-filters/static/media/deadstar.76de0b16.png"
+  }
+]);
